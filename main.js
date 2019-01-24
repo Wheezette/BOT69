35073eebc507c477c7dfe1ab703c2ca5dc6f0dd2
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-01-24 16:37:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -12,42 +12,42 @@ moment.locale('pl');
 	setInterval (function (){
 	  var statusrand  = Math.floor(Math.random() * 8 + 1);
 	  if (statusrand === 1) {
-		bot.user.setActivity(`Zobacz: Topic Shop™`);   
+		bot.user.setActivity(`Discord Servers`);   
 		//bot.channels.get("490431842424717322").setName(moment.utc(message.createdAt).format('HH:mm:ss'));
 		console.log(statusrand);
 	  }
 	  if (statusrand === 2) {
-		bot.user.setActivity(`Użyj: $help`);
+		bot.user.setActivity(`Czekamy na Ciebie!`);
 		//bot.channels.get("490431842424717322").setName(moment.utc(message.createdAt).format('HH:mm:ss'));
 		console.log(statusrand);
 	  }
 	  if (statusrand === 3) {
-		bot.user.setActivity(`Witaj na: Topic Hub©`);  
+		bot.user.setActivity(`ÿ #informacje`);  
 		//bot.channels.get("490431842424717322").setName(moment.utc(message.createdAt).format('HH:mm:ss'));
 		console.log(statusrand);
 	  }
 	  if (statusrand === 4) {
-		bot.user.setActivity(`Zdobądź: Topic Points™`);  
+		bot.user.setActivity(`ÿ #zgłoś-swój-serwer`);  
 		//bot.channels.get("490431842424717322").setName(moment.utc(message.createdAt).format('HH:mm:ss'));
 		console.log(statusrand);
 	  }
 	  if (statusrand === 5) {
-		bot.user.setActivity(`Otrzymaj: Prezent`);  
+		bot.user.setActivity(`Zaproś innych!`);  
 		//bot.channels.get("490431842424717322").setName(moment.utc(message.createdAt).format('HH:mm:ss'));
 		console.log(statusrand);
 	  }
 	  if (statusrand === 6) {
-		bot.user.setActivity(`Wykorzystuj: Vouchery`); 
+		bot.user.setActivity(`To Cię nic nie kosztuje!`); 
 		//bot.channels.get("490431842424717322").setName(moment.utc(message.createdAt).format('HH:mm:ss'));
 		console.log(statusrand); 
 	  }
 	  if (statusrand === 7) {
-		bot.user.setActivity(`Bądź z: Topic Hub©`);  
+		bot.user.setActivity(`Dodaj swój serwer!`);  
 		//bot.channels.get("490431842424717322").setName(moment.utc(message.createdAt).format('HH:mm:ss'));
 		console.log(statusrand);
 	  }
 	  if (statusrand === 8) {
-		bot.user.setActivity(`Zbieraj: Topic Points™`);  
+		bot.user.setActivity(`ÿ #informacje`);  
 		//bot.channels.get("490431842424717322").setName(moment.utc(message.createdAt).format('HH:mm:ss'));
 		console.log(statusrand);
 	  }
@@ -69,19 +69,11 @@ moment.locale('pl');
   });
   
   bot.on("guildMemberRemove", member => {
-	  const embed = new Discord.RichEmbed()
-          .setColor("#F08080")
-          .setDescription(`Żegnaj **${member.user.tag}**. \n \nOjoj. Co spowodowało Twoje odejście?\nCzyżbyś postanowił/a wyjechać na inny kontynent?\n \n**Szerokiej drogi! (Odwiedź nas kiedyś).**`)
-          .setFooter("Topic Hub - edycja 2019");
-	  bot.channels.get("522007704357699584").send(embed);
+	  
   });
   
   bot.on("guildMemberAdd", member => {
-	  const embed = new Discord.RichEmbed()
-          .setColor("#FF5733")
-          .setDescription(`Witaj **${member.user.tag}** na **Topic Hub©**. \n \nCzołem! Witaj na naszym osiedlu. \nPrzeczytaj nasze zasady, aby żadnej nie złamać (#regulamin). \nMożesz Wybrać także kilka ciekawych ról (#zarządzaj-rolami). \n\n **Miłych rozmów i zabawy!**`)
-          .setFooter("Topic Hub - edycja 2019") //${bot.guilds.get("454946768723902476").memberCount}**.`)
-	  bot.channels.get("522007704357699584").send(embed);
+	  
   });
 
 //bot.on('message', async message => {
